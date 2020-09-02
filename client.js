f85164d41576711896b3174a2e1493f6a4e3c2b7
takayama <takayama@foxmail.com>
2020-09-03 05:05:12 +0900

    只尝试一次

--- client.js
+++ client.js
@@ -714,9 +714,12 @@ class AndroidClient extends Client {
                 this.removeAllListeners(event_id);
                 message_id = await new Promise((resolve)=>{
                     const id = setTimeout(()=>{
-                        this.logger.info(`可能被风控了，这条消息将尝试作为长消息发送。`);
+                        this.logger.info(`可能被风控了，将尝试作为长消息再发送一次。`);
                         this.removeAllListeners(event_id);
-                        resolve(false);
+                        if (!as_long)
+                            resolve(false);
+                        else
+                            resolve(group_id.toString(16) + "0".repeat(16));
                     }, 1000);
                     this.once(event_id, (a)=>{
                         clearTimeout(id);
