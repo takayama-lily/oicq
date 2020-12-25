f4a6085640a27a055f210ba3654e9007e6e32330
takayama-lily <takayama@foxmail.com>
2020-12-26 06:42:38 +0900

    Update demo.js

--- docs/demo.js
+++ docs/demo.js
@@ -10,20 +10,13 @@ const uin = 123456789;
 const bot = createClient(uin, {
     log_level: "debug", //日志级别设置为debug
     platform: 1, //登录设备选择为手机
-    slider: true, //允许出现滑动验证码
+    slider: true, //打开滑动验证码
 });
 
 //监听并输入滑动验证码ticket
 bot.on("system.login.slider", ()=>{
     process.stdin.once("data", (input)=>{
         bot.sliderLogin(input);
-    });
-});
-
-//监听并输入图片验证码
-bot.on("system.login.captcha", ()=>{
-    process.stdin.once("data", (input)=>{
-        bot.captchaLogin(input);
     });
 });
 
