1f1b4f0efe0412269245ae68bfc4398dc64b5703
takayama <takayama@foxmail.com>
2021-05-18 18:47:09 +0900

    make algo dir, mk core dir move fn to nessy remove stdin

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { Client, createClient } = require("./lib/client");
 const { segment, cqcode } = require("./util");
 module.exports = {
     setGlobalConfig: () => { },
-    stdin: require("./stdin"),
     segment, cqcode,
     Client, createClient,
 };
