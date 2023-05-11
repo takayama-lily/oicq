56d18fd02271621803dfd1914ec9b28b8e54fba0
Dimole <dmlgzs@qq.com>
2023-05-11 10:44:13 +0800

    fix: 修复登录无限 token 失效 (#523)

--- lib/core/tlv.ts
+++ lib/core/tlv.ts
@@ -26,7 +26,7 @@ const map: { [tag: number]: (this: BaseClient, ...args: any[]) => Writer } = {
 			.writeU16(1) // ip ver
 			.writeBytes(crypto.randomBytes(4))
 			.writeU32(this.uin)
-			.write32(Date.now() & 0xffffffff)
+			.write32((Date.now() / 1000) & 0xffffffff)
 			.writeBytes(ipB) //ip
 			.writeU16(0)
 	},
@@ -112,7 +112,7 @@ const map: { [tag: number]: (this: BaseClient, ...args: any[]) => Writer } = {
 			.writeU32(this.apk.appid)
 			.writeU32(0) // app client ver
 			.writeU64(this.uin)
-			.write32(Date.now() & 0xffffffff)
+			.write32((Date.now() / 1000) & 0xffffffff)
 			.writeBytes(Buffer.alloc(4)) // dummy ip
 			.writeU8(1) // save password
 			.writeBytes(md5pass)
@@ -265,7 +265,7 @@ const map: { [tag: number]: (this: BaseClient, ...args: any[]) => Writer } = {
 			.writeBytes(crypto.randomBytes(16))
 			.write32(1)
 			.write32(16)
-			.write32(Date.now() & 0xffffffff)
+			.write32((Date.now() / 1000) & 0xffffffff)
 			.writeBytes(Buffer.alloc(0))
 	},
 	0x401: function () {
